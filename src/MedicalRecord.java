5278b060dd0f888d281669d86e6b0e968b4036cd
Arushi Verma <157723989+aru6hi@users.noreply.github.com>
2024-10-27 04:05:48 +0800

    Added MedicalRecord and MedicalRecordMgr class

---
+++ src/MedicalRecord.java
@@ -0,0 +1,59 @@
+public class MedicalRecord {
+    private static int numOfRecords = 0; 
+    private Patient patient; 
+    private String diagnoses;
+    private String medication;
+    private String pastTreatments;
+
+    // Constructor to initialize fields
+    public MedicalRecord() {
+        this.diagnoses = "EMPTY"; 
+        this.medication = "EMPTY";
+        this.pastTreatments = "EMPTY";
+    }
+
+    // Getters and Setters
+    public void setPatient(Patient patient) {
+        this.patient = patient;
+    }
+
+    public void setDiagnoses(String diagnoses) {
+        this.diagnoses = diagnoses;
+    }
+
+    public void setMedication(String medication) {
+        this.medication = medication;
+    }
+
+    public void setPastTreatments(String pastTreatments) {
+        this.pastTreatments = pastTreatments;
+    }
+
+    public String getDiagnoses() {
+        return this.diagnoses;
+    }
+
+    public String getMedication() {
+        return this.medication;
+    }
+
+    public String getPastTreatments() {
+        return this.pastTreatments;
+    }
+
+    public Patient getPatient() {
+        return this.patient;
+    }
+
+    public static int getNumOfRecords() {
+        return numOfRecords;
+    }
+
+    public int getPatientID() {
+        return patient.getPatientID();
+    }
+
+    public static void incrementRecords() {
+        numOfRecords++;
+    }
+}
---
+++ src/MedicalRecordMgr.java
@@ -0,0 +1,87 @@
+import java.util.Scanner;
+
+public class MedicalRecordMgr {
+    private MedicalRecord[] medicalRecords;
+
+    public MedicalRecordMgr() {
+        medicalRecords = new MedicalRecord[100]; 
+    }
+
+    // Find a medical record by patient ID
+    public MedicalRecord findByID(int patientID) {
+        if (medicalRecords == null || MedicalRecord.getNumOfRecords() == 0) return null;
+        
+        for (MedicalRecord record : medicalRecords) {
+            if (record != null && record.getPatientID() == patientID) {
+                return record;
+            }
+        }
+        return null;
+    }
+
+    // Add a medical record
+    public void addRecord(Patient patient, String diagnoses, String medication, String pastTreatments) {
+        MedicalRecord mRecord = new MedicalRecord();
+        mRecord.setPatient(patient);
+        mRecord.setDiagnoses(diagnoses);
+        mRecord.setMedication(medication);
+        mRecord.setPastTreatments(pastTreatments);
+        
+        medicalRecords[MedicalRecord.getNumOfRecords()] = mRecord;
+        MedicalRecord.incrementRecords(); 
+    }
+
+    // Print a medical record
+    public void printRecord(MedicalRecord medRecord) {
+        if (medRecord == null) {
+            System.out.println("No record found.");
+            return;
+        }
+
+        System.out.println("Printing medical records for Patient ID: " + medRecord.getPatientID());
+        Patient patient = medRecord.getPatient();
+        System.out.println("Patient Name: " + patient.getName());
+        System.out.println("Patient Age: " + patient.getAge());
+        System.out.println("Diagnoses: " + medRecord.getDiagnoses());
+        System.out.println("Medications: " + medRecord.getMedication());
+        System.out.println("Past Treatments: " + medRecord.getPastTreatments());
+    }
+
+    // Edit a medical record
+    public void editRecord(MedicalRecord medRecord) {
+        if (medRecord == null) {
+            System.out.println("No record found to edit.");
+            return;
+        }
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Choose one of the following: ");
+        System.out.println("1. Edit Patient Diagnoses");
+        System.out.println("2. Edit Patient Medications");
+        System.out.println("3. Edit Patient Past Treatments");
+
+        int option = sc.nextInt();
+        sc.nextLine(); 
+
+        switch(option) {
+            case 1:
+                System.out.println("Enter new diagnoses: ");
+                String diag = sc.nextLine();
+                medRecord.setDiagnoses(diag);
+                break;
+            case 2:
+                System.out.println("Enter new medications: ");
+                String med = sc.nextLine();
+                medRecord.setMedication(med);
+                break;
+            case 3:
+                System.out.println("Enter new past treatments: ");
+                String treatment = sc.nextLine();
+                medRecord.setPastTreatments(treatment); 
+                break;
+            default:
+                System.out.println("Invalid option");
+        }
+    }
+}
