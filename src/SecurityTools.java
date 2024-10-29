0f743057ff1fb7a729dc5b8fd4537471adc4e0fc
Raye <59753706+MoiShroomy@users.noreply.github.com>
2024-10-29 22:46:45 +0800

    Fixed a minor bug regarding static methods

--- src/SecurityTools.java
+++ src/SecurityTools.java
@@ -1,22 +1,23 @@
 public final class SecurityTools {
-	char key = 'Q';
+	
 
 	private SecurityTools() {};
 	
 	public static String encryptPassword(String rawPassword) {
 		//A simple XOR cipher for encrypting passwords
 		//Run it again on an encrypted password to decrypt
+		char key = 'Q';
 		String out = ""; 
 		  
-        int length = rawPassword.length(); 
+        	int length = rawPassword.length(); 
 
-        for (int i = 0; i < length; i++)  
-        { 
-            out = out + Character.toString((char) (rawPassword.charAt(i) ^ key)); 
-        }
-        
-        //System.out.println(out);
-        
-        return out;
+	        for (int i = 0; i < length; i++)  
+	        { 
+	            out = out + Character.toString((char) (rawPassword.charAt(i) ^ key)); 
+	        }
+	        
+	        //System.out.println(out);
+	        
+	        return out;
 	}
 }
